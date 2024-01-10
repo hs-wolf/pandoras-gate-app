253912aa8be33445c67ae43d3e2e322dda705206
José Israel <hswolf@proton.me>
2024-01-09 23:48:43 -0300

    Pinia integration.
    Added the pinia and @pinia/nuxt packages.
    
    Created a mocked alerts store.
    
    Created a constants file under utils.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true
+  },
   srcDir: 'src',
   modules: [
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@pinia/nuxt'
   ],
   eslint: {
     lintOnStart: false
+  },
+  pinia: {
+    storesDirs: ['stores/**']
   }
 })
