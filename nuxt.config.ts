d777fd66c996d0bea3c4877c5ece83236d049ec0
José Israel <hswolf@proton.me>
2024-01-11 21:22:54 -0300

    Cleaned up comments and updated Readme.
    Included app header in the nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {
     enabled: true
   },
   srcDir: 'src',
   css: ['assets/css/main.scss'],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      },
+      title: 'pandora-gate-app',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'client' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
   modules: [
     '@nuxtjs/eslint-module',
     '@pinia/nuxt',
