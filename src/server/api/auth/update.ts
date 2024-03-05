d682da4cd900b4876d88d6103eef80fc54ca9caa
José Israel <hswolf@proton.me>
2024-03-05 00:01:29 -0300

    Created the auth middleware to get the uid and the claims of the user.
    Created the auth/delete and auth/update endpoints.

---
+++ src/server/api/auth/update.ts
@@ -0,0 +1,8 @@
+import { getAuth } from 'firebase-admin/auth'
+import { AuthUserRegisterBody } from '~/types'
+
+export default defineEventHandler(async (event) => {
+  const auth = getAuth()
+  const body = await readBody<AuthUserRegisterBody>(event)
+  return auth.createUser(body)
+})
---
+++ src/server/middleware/auth.ts
@@ -0,0 +1,12 @@
+import { getAuth } from 'firebase-admin/auth'
+
+export default defineEventHandler(async (event) => {
+  const auth = getAuth()
+  const headers = getHeaders(event)
+  const rawToken = headers.authorization
+  const cleanToken = rawToken
+    ? rawToken.replace('Bearer', '')
+    : ''
+  const claims = await auth.verifyIdToken(cleanToken)
+  event.context.auth = { uid: claims.id, claims }
+})
